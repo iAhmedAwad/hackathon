d6caa09cedfb440502dd9b43ddbcce1c24f1ebee
Awad <mmawad.ahmed@gmail.com>
2023-12-02 16:23:20 +0200

    Create SendingMessageDomainModel.kt

---
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/domain/models/SendingMessageDomainModel.kt
@@ -0,0 +1,7 @@
+package com.trianglz.chatbot.modules.chat.domain.models
+
+/**
+ * @author Awad for TrianglZ
+ * Created 12/2/2023 at 1:09 PM
+ */
+class SendingMessageDomainModel(val message: String)
