f2470ed3001f7885371b2c750c98bdc9320ca34a
Awad <mmawad.ahmed@gmail.com>
2023-12-02 18:31:10 +0200

    Update ChatScreenContent.kt

--- chatbot/src/main/java/com/trianglz/chatbot/modules/chat/presentation/composables/ChatScreenContent.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/presentation/composables/ChatScreenContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.chatbot_sdk.modules.chat.domain.models.enum.SenderType
 import com.trianglz.chatbot.modules.chat.presentation.models.ChatUIModel
 
@@ -15,7 +16,12 @@ import com.trianglz.chatbot.modules.chat.presentation.models.ChatUIModel
 @Composable
 fun ChatScreenContent(
     chatList: State<SnapshotStateList<ChatUIModel>>,
-    onSendClicked: (text: String) -> Unit
+    onSendClicked: (text: String) -> Unit,
+    primaryColor: () -> Color,
+    userChatColor: () -> Color,
+    botChatColor: () -> Color,
+    userBackgroundColor: () -> Color,
+    botBackgroundColor: () -> Color
 ) {
 
     Column(modifier = Modifier.fillMaxSize()) {
@@ -23,13 +29,20 @@ fun ChatScreenContent(
         LazyColumn(Modifier.weight(1F)) {
             items(chatList.value.toList()) { message ->
                 when (message.type) {
-                    SenderType.ME -> MeTextMessageItem(chatUIModel = message)
-                    SenderType.OTHER -> OtherTextMessageItem(chatUIModel = message)
+                    SenderType.ME -> MeTextMessageItem(
+                        chatUIModel = message,
+                        userChatColor = userChatColor, userBackgroundColor = userBackgroundColor
+                    )
+
+                    SenderType.OTHER -> OtherTextMessageItem(
+                        chatUIModel = message,
+                        botChatColor = botChatColor, botBackgroundColor = botBackgroundColor
+                    )
                 }
             }
         }
 
-        ChatDetailsActionsItem(onSendClicked = onSendClicked)
+        ChatDetailsActionsItem(onSendClicked = onSendClicked, primaryColor = primaryColor)
     }
 
 }
