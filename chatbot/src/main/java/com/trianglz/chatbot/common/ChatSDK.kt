cd737f257f918266322b3babf1c23de550071163
Awad <mmawad.ahmed@gmail.com>
2023-12-02 18:31:06 +0200

    Update ChatSDK.kt

--- chatbot/src/main/java/com/trianglz/chatbot/common/ChatSDK.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/common/ChatSDK.kt
@@ -1,11 +1,26 @@
 package com.trianglz.chatbot.common
 
+import androidx.compose.ui.graphics.Color
+
 
 object SDK {
+    var primaryColor: Color = Color.Red
+    var userChatColor: Color = Color.White
+    var botChatColor: Color = Color.White
+    var botBackgroundColor: Color = Color.White
+    var userBackgroundColor: Color = Color.White
 
-    lateinit var name: String
-
-    fun init(name: String) {
-        this.name = name
+    fun init(
+        primaryColor: Color,
+        userChatColor: Color = Color.White,
+        botChatColor: Color = Color.White,
+        botBackgroundColor: Color = Color.White,
+        userBackgroundColor: Color = Color.White,
+    ) {
+        this.primaryColor = primaryColor
+        this.userChatColor = userChatColor
+        this.botChatColor = botChatColor
+        this.botBackgroundColor = botBackgroundColor
+        this.userBackgroundColor = userBackgroundColor
     }
 }
