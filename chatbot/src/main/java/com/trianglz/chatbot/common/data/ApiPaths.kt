23b4b4b8b1fe767afd0db6cc4a51c752ab15f9b2
Awad <mmawad.ahmed@gmail.com>
2023-12-02 16:22:55 +0200

    Create ApiPaths.kt

---
+++ chatbot/src/main/java/com/trianglz/chatbot/common/data/ApiPaths.kt
@@ -0,0 +1,11 @@
+package com.trianglz.chatbot.common.data
+
+/**
+ * @author Awad for TrianglZ
+ * Created 12/2/2023 at 3:46 PM
+ */
+class ApiPaths {
+    companion object {
+        const val THE_MATE = "the_mate"
+    }
+}
