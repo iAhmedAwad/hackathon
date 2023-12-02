8d04fe321ee7b9c7c5409cbd08300c0d6d38be46
Awad <mmawad.ahmed@gmail.com>
2023-12-02 16:23:01 +0200

    Create SendingMessageRequestModel.kt

---
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/data/remote/retrofit/models/request/SendingMessageRequestModel.kt
@@ -0,0 +1,7 @@
+package com.trianglz.chatbot.modules.chat.data.remote.retrofit.models.request
+
+/**
+ * @author Awad for TrianglZ
+ * Created 12/2/2023 at 1:05 PM
+ */
+data class SendingMessageRequestModel(val message: String)
