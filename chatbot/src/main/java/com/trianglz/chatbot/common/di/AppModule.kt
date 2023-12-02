a6e17fc96e4c6529493856932a8fd30038290c63
Awad <mmawad.ahmed@gmail.com>
2023-12-02 16:22:57 +0200

    Create AppModule.kt

---
+++ chatbot/src/main/java/com/trianglz/chatbot/common/di/AppModule.kt
@@ -0,0 +1,80 @@
+package com.trianglz.chatbot.common.di
+
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    //region Network
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(): Retrofit.Builder = Retrofit.Builder()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClientBuilder(): OkHttpClient.Builder = OkHttpClient.Builder()
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(
+        builder: OkHttpClient.Builder,
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("156.163.110.170",8080))
+
+        return   builder.addNetworkInterceptor(StethoInterceptor())
+            .addInterceptor(httpLoggingInterceptor).proxy(proxy)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    //    @Singleton
+    @Provides
+    fun provideRetrofit(
+        httpClient: OkHttpClient,
+        builder: Retrofit.Builder,
+        moshi: Moshi,
+        //baseUrl: String = BASEURL
+    ): Retrofit =
+        builder
+            .baseUrl(BASEURL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(httpClient)
+            .build()
+    //endregion
+
+
+    companion object {
+        const val BASEURL = "https://huge-lands-think.loca.lt/"
+    }
+}
