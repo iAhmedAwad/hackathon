8d724e34a16e9bfdd792deba4e6e4f226fa562c5
Awad <mmawad.ahmed@gmail.com>
2023-12-02 16:22:56 +0200

    Create ApiQueries.kt

---
+++ chatbot/src/main/java/com/trianglz/chatbot/common/data/ApiQueries.kt
@@ -0,0 +1,11 @@
+package com.trianglz.chatbot.common.data
+
+/**
+ * @author Awad for TrianglZ
+ * Created 12/2/2023 at 3:46 PM
+ */
+class ApiQueries {
+    companion object {
+        const val USER_INPUT = "user_input"
+    }
+}
