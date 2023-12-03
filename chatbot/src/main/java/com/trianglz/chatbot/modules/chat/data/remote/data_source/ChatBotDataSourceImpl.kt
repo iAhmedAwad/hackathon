95b5cc48c9a99b4428e3ac647c3ce075a2092b7f
Awad <mmawad.ahmed@gmail.com>
2023-12-03 07:26:57 +0200

    Update ChatBotDataSourceImpl.kt

--- chatbot/src/main/java/com/trianglz/chatbot/modules/chat/data/remote/data_source/ChatBotDataSourceImpl.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/data/remote/data_source/ChatBotDataSourceImpl.kt
@@ -1,22 +1,19 @@
 package com.trianglz.chatbot.modules.chat.data.remote.data_source
 
-import com.example.chatbot_sdk.modules.chat.domain.models.enum.SenderType
+import com.trianglz.chatbot.common.data.service.APIService
 import com.trianglz.chatbot.modules.chat.data.mapper.toData
 import com.trianglz.chatbot.modules.chat.data.models.ChatDataModel
 import com.trianglz.chatbot.modules.chat.data.remote.retrofit.models.request.SendingMessageRequestModel
-import com.trianglz.chatbot.modules.chat.data.remote.retrofit.service.ChatBotService
-import kotlinx.coroutines.delay
-import javax.inject.Inject
 
 /**
  * @author Awad for TrianglZ
  * Created 12/2/2023 at 12:20 PM
  */
-class ChatBotDataSourceImpl @Inject constructor(private val service: ChatBotService) : ChatBotDataSource {
+class ChatBotDataSourceImpl : ChatBotDataSource {
+
+    private val service = APIService.chatBotService
     override suspend fun sendMessage(message: SendingMessageRequestModel): ChatDataModel {
 
-        //delay(2000)
-        //return ChatDataModel(System.identityHashCode(this),"this is the bot response...",SenderType.OTHER)
-        return  service.sendMessage(message.message).toData()
+        return service.sendMessage(message.message).toData()
     }
 }
