4bb63f9b16595a1d884859d95d79c8df40bea572
Awad <mmawad.ahmed@gmail.com>
2023-12-03 07:26:59 +0200

    Update SendMessageUseCase.kt

--- chatbot/src/main/java/com/trianglz/chatbot/modules/chat/domain/usecase/SendMessageUseCase.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/domain/usecase/SendMessageUseCase.kt
@@ -1,16 +1,16 @@
 package com.trianglz.chatbot.modules.chat.domain.usecase
 
+import com.trianglz.chatbot.modules.chat.data.repo.ChatBotRepoImpl
 import com.trianglz.chatbot.modules.chat.domain.models.ChatDomainModel
 import com.trianglz.chatbot.modules.chat.domain.models.SendingMessageDomainModel
-import com.trianglz.chatbot.modules.chat.domain.repo.ChatBotRepo
-import javax.inject.Inject
 
 /**
  * @author Awad for TrianglZ
  * Created 12/2/2023 at 1:17 PM
  */
-class SendMessageUseCase @Inject constructor(private val repo: ChatBotRepo) {
+class SendMessageUseCase {
 
+    private val repo = ChatBotRepoImpl()
     suspend fun execute(message: SendingMessageDomainModel): ChatDomainModel {
         return repo.sendMessage(message = message)
     }
