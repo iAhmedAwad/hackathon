5103a3f4de9edb2de65c5dc193c57431baf7aced
Awad <mmawad.ahmed@gmail.com>
2023-12-03 07:26:57 +0200

    Update ChatBotRepoImpl.kt

--- chatbot/src/main/java/com/trianglz/chatbot/modules/chat/data/repo/ChatBotRepoImpl.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/modules/chat/data/repo/ChatBotRepoImpl.kt
@@ -2,17 +2,17 @@ package com.trianglz.chatbot.modules.chat.data.repo
 
 import com.trianglz.chatbot.modules.chat.data.mapper.toDomain
 import com.trianglz.chatbot.modules.chat.data.mapper.toRequest
-import com.trianglz.chatbot.modules.chat.data.remote.data_source.ChatBotDataSource
+import com.trianglz.chatbot.modules.chat.data.remote.data_source.ChatBotDataSourceImpl
 import com.trianglz.chatbot.modules.chat.domain.models.ChatDomainModel
 import com.trianglz.chatbot.modules.chat.domain.models.SendingMessageDomainModel
 import com.trianglz.chatbot.modules.chat.domain.repo.ChatBotRepo
-import javax.inject.Inject
 
 /**
  * @author Awad for TrianglZ
  * Created 12/2/2023 at 1:11 PM
  */
-class ChatBotRepoImpl @Inject constructor(private val dataSource: ChatBotDataSource) : ChatBotRepo {
+class ChatBotRepoImpl : ChatBotRepo {
+    private val dataSource = ChatBotDataSourceImpl()
     override suspend fun sendMessage(message: SendingMessageDomainModel): ChatDomainModel {
         return dataSource.sendMessage(message.toRequest()).toDomain()
     }
