b9d484ca102b45e5dec6a25bbad9a24facd9a19d
Awad <mmawad.ahmed@gmail.com>
2023-12-03 07:26:54 +0200

    Update ChatBotApp.kt

--- chatbot/src/main/java/com/trianglz/chatbot/common/ChatBotApp.kt
+++ chatbot/src/main/java/com/trianglz/chatbot/common/ChatBotApp.kt
@@ -1,8 +1,5 @@
 package com.trianglz.chatbot.common
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
 /**
  * @author Awad for TrianglZ
  * Created 11/29/2023 at 2:19 PM
